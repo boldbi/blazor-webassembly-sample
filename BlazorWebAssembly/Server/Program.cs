6089d9f329587ccb4a744a9039eff9e8aeed4c9e
Nithya Gopal <nithya.gopal@syncfusion.com>
2023-05-04 15:50:54 +0530

    309294-Configure-embedConfigjson-instead-of-EmbedProperties-in-BlazorWebassemblySample

--- BlazorWebAssembly/Server/Program.cs
+++ BlazorWebAssembly/Server/Program.cs
@@ -1,26 +1,81 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BlazorWebAssembly.Models;
+using BlazorWebAssembly.Shared;
+using BlazorWebAssembly.Server;
+using Microsoft.AspNetCore.ResponseCompression;
+using Newtonsoft.Json;
+using System.Reflection;
 
-namespace BlazorWebAssembly.Server
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseWebAssemblyDebugging();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+
+try
+{
+    string basePath = AppDomain.CurrentDomain.BaseDirectory;
+    string jsonString = System.IO.File.ReadAllText(Path.Combine(basePath, "embedConfig.json"));
+    GlobalAppSettings.EmbedDetails = JsonConvert.DeserializeObject<EmbedDetails>(jsonString);
+}
+
+//try
+//{
+//    string basePath = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+//string jsonFilePath = Path.Combine(basePath, "Shared", "embedConfig.json");
+//    string jsonString = System.IO.File.ReadAllText(Path.Combine(basePath, "embedConfig.json"));
+//   // string jsonString = File.ReadAllText(jsonFilePath);
+//GlobalAppSettings.EmbedDetails = JsonConvert.DeserializeObject<EmbedDetails>(jsonString);
+//}
+catch
+{
+    app.MapFallbackToFile("EmbedConfigErrorLog");
+}
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapFallbackToController("EmbedConfigErrorLog", "EmbedData");
+});
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=EmbedData}/{action=EmbedConfigErrorLog}/{id?}");
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=EmbedData}/{action=EmbedConfig}/{id?}");
+
+//app.MapFallbackToFile("Host1.cshtml");
+
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapBlazorHub();
+//    endpoints.MapFallbackToPage("/_Host");
+//});
+
+app.Run();
