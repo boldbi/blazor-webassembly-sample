c3e3e268dbc4804de66c040db631a24e6e427367
Nithya Gopal <nithya.gopal@syncfusion.com>
2023-05-04 16:30:38 +0530

    Program.cs file changes

--- BlazorWebAssembly/Server/Program.cs
+++ BlazorWebAssembly/Server/Program.cs
@@ -37,45 +37,9 @@ app.UseRouting();
 app.MapRazorPages();
 app.MapControllers();
 
-try
-{
-    string basePath = AppDomain.CurrentDomain.BaseDirectory;
-    string jsonString = System.IO.File.ReadAllText(Path.Combine(basePath, "embedConfig.json"));
-    GlobalAppSettings.EmbedDetails = JsonConvert.DeserializeObject<EmbedDetails>(jsonString);
-}
-
-//try
-//{
-//    string basePath = Path.GetDirectoryName(typeof(Program).Assembly.Location);
-//string jsonFilePath = Path.Combine(basePath, "Shared", "embedConfig.json");
-//    string jsonString = System.IO.File.ReadAllText(Path.Combine(basePath, "embedConfig.json"));
-//   // string jsonString = File.ReadAllText(jsonFilePath);
-//GlobalAppSettings.EmbedDetails = JsonConvert.DeserializeObject<EmbedDetails>(jsonString);
-//}
-catch
-{
-    app.MapFallbackToFile("EmbedConfigErrorLog");
-}
-
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapFallbackToController("EmbedConfigErrorLog", "EmbedData");
 });
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=EmbedData}/{action=EmbedConfigErrorLog}/{id?}");
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=EmbedData}/{action=EmbedConfig}/{id?}");
-
-//app.MapFallbackToFile("Host1.cshtml");
-
-//app.UseEndpoints(endpoints =>
-//{
-//    endpoints.MapBlazorHub();
-//    endpoints.MapFallbackToPage("/_Host");
-//});
 
 app.Run();
