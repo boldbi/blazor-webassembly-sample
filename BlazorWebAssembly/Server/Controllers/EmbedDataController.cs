225687c35ef8470a57ce9216775ca1f1a2e48f70
Nithya <nithya.gopal@syncfusion.com>
2023-06-20 15:50:02 +0530

    309294-Configure-embedConfigjson-instead-of-EmbedProperties-in-BlazorWebassemblySample

---
+++ BlazorWebAssembly/Server/Controllers/EmbedDataController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using BlazorWebAssembly.Shared;
+using BlazorWebAssembly.Client.Pages;
+
+namespace BlazorWebAssembly.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmbedDataController : Controller
+    {
+        [HttpGet]
+        [Route("GetConfig")]
+        public IActionResult GetConfig()
+        {
+            var jsonData = System.IO.File.ReadAllText("embedConfig.json");
+            string basePath = AppDomain.CurrentDomain.BaseDirectory;
+            string jsonString = System.IO.File.ReadAllText(Path.Combine(basePath, "embedConfig.json"));
+            GlobalAppSettings.EmbedDetails = JsonConvert.DeserializeObject<EmbedDetails>(jsonString);
+            return Ok(jsonData);
+        }
+
+        [HttpGet]
+        [Route("EmbedConfigErrorLog")]
+        public IActionResult EmbedConfigErrorLog()
+        {
+            return View("~/Pages/EmbedConfigErrorLog.cshtml");
+        }
+
+        [HttpPost("[action]")]
+        [Route("AuthorizationServer")]
+        public string AuthorizationServer([FromBody] object embedQuerString)
+        {
+            var embedClass = JsonConvert.DeserializeObject<EmbedClass>(embedQuerString.ToString());
+            var embedQuery = embedClass.embedQuerString;
+            embedQuery += "&embed_user_email=" + GlobalAppSettings.EmbedDetails.UserEmail;
+            //To set embed_server_timestamp to overcome the EmbedCodeValidation failing while different timezone using at client application.
+            double timeStamp = (int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+            embedQuery += "&embed_server_timestamp=" + timeStamp;
+            var embedDetailsUrl = "/embed/authorize?" + embedQuery + "&embed_signature=" + GetSignatureUrl(embedQuery);
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(embedClass.dashboardServerApiUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+
+                var result = client.GetAsync(embedClass.dashboardServerApiUrl + embedDetailsUrl).Result;
+                string resultContent = result.Content.ReadAsStringAsync().Result;
+                return resultContent;
+            }
+        }
+
+        public string GetSignatureUrl(string message)
+        {
+            var encoding = new System.Text.UTF8Encoding();
+            var keyBytes = encoding.GetBytes(GlobalAppSettings.EmbedDetails.EmbedSecret);
+            var messageBytes = encoding.GetBytes(message);
+            using (var hmacsha1 = new System.Security.Cryptography.HMACSHA256(keyBytes))
+            {
+                var hashMessage = hmacsha1.ComputeHash(messageBytes);
+                return Convert.ToBase64String(hashMessage);
+            }
+        }
+    }
+}
